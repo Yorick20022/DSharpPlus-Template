654a7713ec154c6479a0d59ee6c1cf5ffa877651
Yorick <125297057+Yorick20022@users.noreply.github.com>
2024-06-30 17:07:44 +0200

    Removed prefix code, slash commands better

--- Config/ConfigJsonReader.cs
+++ Config/ConfigJsonReader.cs
@@ -5,10 +5,9 @@ namespace DiscordBotTutorialExampleProject.Config
 {
     public class ConfigJsonReader
     {
-        //Declare our Token & Prefix properties of this class
+        //Declare our Token properties of this class
         public string discordToken { get; set; }
-        public string discordPrefix { get; set; }
-
+        
         public async Task ReadJSON() //This method must be run asynchronously
         {
             //Initialise a StreamReader
@@ -22,13 +21,11 @@ namespace DiscordBotTutorialExampleProject.Config
 
             //Set the properties
             this.discordToken = obj.token;
-            this.discordPrefix = obj.prefix;
         }
     }
 
     internal sealed class JSONStruct
     {
         public string token { get; set; }
-        public string prefix { get; set; }
     }
 }
